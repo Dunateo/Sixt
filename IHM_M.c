f21b1d12286a3a8e4dca0a82b115c77536367d85
MathiasBoillot <mathias.boillot@gmail.com>
2019-06-20 18:02:00 +0200

    IHM Vehicule fini sauf revision et historique

--- IHM_M.c
+++ IHM_M.c
@@ -12,19 +12,21 @@ typedef struct variable {
   char***voiture_det;
   GtkWidget*label1[11];
   int i;
+  maillon*ptr;
 }variable;
 
-void InitVar(variable*var) {
+void InitVar(variable*var, maillon*ptrTete) {
   var->i = 0;
   var->voiture_det = (char***) malloc(sizeof(char**)*DIM); // Nombre de voiture + 1 (car le nombre de voiture commence a 1)
   for(int j = 0; j<DIM;j++) {
     var->voiture_det[j] = (char**) malloc(sizeof(char*)*11); // Nombre d'attribut
     for(int i = 0; i<11; i++) {
-      var->voiture_det[j][i] = (char*) malloc(sizeof(char)*20); // Nombre de caractère
+      var->voiture_det[j][i] = (char*) malloc(sizeof(char)*30); // Nombre de caractère
     }
   }
   for(int i=0; i<=11; i++)
   var->label1[i] = NULL;
+  var->ptr = ptrTete;
 }
 
 
@@ -63,27 +65,46 @@ void print_hello(GtkWidget*widget, gpointer data) {
   var->i = atoi(gtk_widget_get_tooltip_text(widget)); // pour récuperer la voiture sur laquelle on clique
 }
 
+void initVoitureDetail(variable*var) {
 
+  maillon*ptr = var->ptr;
+  int i = 1;
+
+  while(ptr->suivant != NULL ){
+
+    if(ptr->rent->u.value_car->gearbox == true)
+      var->voiture_det[i][5] = "Automatic"; // gearbox
+    else
+      var->voiture_det[i][5] = "Manuel"; // gearbox
+
+    strcpy(var->voiture_det[i][0], ptr->rent->u.value_car->plate_number); //plate number
+    sprintf(var->voiture_det[i][1],"%c", ptr->rent->u.value_car->category);  //category
+    sprintf(var->voiture_det[i][2],"%d", ptr->rent->u.value_car->km); //km
+    strcpy(var->voiture_det[i][3], ptr->rent->u.value_car->brand_name); //brand
+    strcpy(var->voiture_det[i][4], ptr->rent->u.value_car->brand_model); // model
+    sprintf(var->voiture_det[i][6],"%d",ptr->rent->u.value_car->car_year); // years
+    //var->voiture_det[i][7] = "plate"; // revision
+    sprintf(var->voiture_det[i][8] , "%.f€", dailyPrice(ptr->rent->u.value_car->category)); // daily price
+    sprintf(var->voiture_det[i][9] ,"%.f€", sellingCar(ptr->rent)); // selling price
+    sprintf(var->voiture_det[i][10] ,"%.f€",ptr->rent->u.value_car->price); // purchase price
+
+    i++;
+    ptr = ptr->suivant;
+
+  }
+}
 
 void edit_label(GtkWidget*widget, gpointer data) {
 
   variable*var = data;
-
+  maillon*ptr = var->ptr;
   var->i = atoi(gtk_widget_get_tooltip_text(widget)); // pour récuperer la voiture sur laquelle on clique
 
   // Contient les attributs
 
-  var->voiture_det[var->i][0] = "88-MFL-88"; //plate number
-  var->voiture_det[var->i][1] = "A";  //category
-  var->voiture_det[var->i][2] = "175862"; //km
-  var->voiture_det[var->i][3] = "Citröen"; //brand
-  var->voiture_det[var->i][4] = "C5"; // model
-  var->voiture_det[var->i][5] = "Manuel"; // gearbox
-  var->voiture_det[var->i][6] = "2016"; // years
-  var->voiture_det[var->i][7] = "plate"; // revision
-  var->voiture_det[var->i][8] = "30€"; // daily price
-  var->voiture_det[var->i][9] = "11999€"; // selling price
-  var->voiture_det[var->i][10] = "20999€"; // purchase price
+
+
+
 
 
   // Change le contenu du label
@@ -123,43 +144,70 @@ void attributVehicule(variable*var, GtkBuilder*p_builder) {
 
 
 void GenerateVehicule(variable*var, GtkBuilder*p_builder, maillon*ptrTete ) {
-    GtkWidget*box_car = NULL; // La GtkBox contenant toute les voitures
-    GtkWidget*car[DIM]; // Correspond au voiture a afficher, elle est ajouté dans box_car
-    GtkWidget*tab[2];// Contient le contenu de GtkBox  de véhicule
-    char number[4]; // Recupere le nombre de voiture pour le concaténer
-    maillon*ptr = ptrTete;
-    char*model_car;
+  GtkWidget*box_car = NULL; // La GtkBox contenant toute les voitures
+  GtkWidget*car[DIM]; // Correspond au voiture a afficher, elle est ajouté dans box_car
+  GtkWidget*tab[2];// Contient le contenu de GtkBox  de véhicule
+  char number[4]; // Recupere le nombre de voiture pour le concaténer
+  maillon*ptr = ptrTete;
+  char*model_car;
+  char*brand_car;
+  int i = 1;
+  char*cwd;
 
-    model_car = (char*) malloc(sizeof(char)*(strlen(ptr->rent->u.value_car->brand_name)+5));
+  cwd = (char*) malloc(sizeof(char)*60);
+
+  cwd = getcwd(cwd, 100);
 
   box_car = GTK_WIDGET(gtk_builder_get_object(p_builder, "box_car")); // Permet de définir la GtkBox de véhicule
 
-  for(int i=1; i<100; i++) { // Boucle selon le nombre de voiture
+  initVoitureDetail(var);
 
+  while(ptr->suivant != NULL){ // Boucle selon le nombre de voiture
+
+
+    model_car = (char*) malloc(sizeof(char)*(strlen(ptr->rent->u.value_car->brand_model)+1));
+    brand_car = (char*) malloc(sizeof(char)*(strlen(ptr->rent->u.value_car->brand_name)+55));
     sprintf(number,"%d",i);
 
-    strcpy(model_car,ptr->rent->u.value_car->brand_name);
-    strcat(model_car, ".png");
+    strcpy(model_car, ptr->rent->u.value_car->brand_model);
 
-    printf("\n\n\n%s\n\n", model_car);
+    strcpy(brand_car,cwd);
+    strcat(brand_car, "/Pictures/");
+    strcat(brand_car, ptr->rent->u.value_car->brand_name);
+    strcat(brand_car, ".png");
+
+
+
+
+
+
+
+
+    /*  printf("\n\n\n%s\n\n", brand_car);*/
 
     car[i] = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5); // Param 1 : orientation, param 2 : nb de pixel qui separe les elements
 
-    tab[0] =gtk_image_new_from_file ("Citroen.png"); // Initialise le logo de la marque
-    tab[1] = gtk_label_new("C5"); // Initialise le modele de voiture
-    tab[2] =  gtk_button_new_with_label ("Show"); // Initialise le bouton
+
+    tab[0] = gtk_image_new_from_file (brand_car); // Initialise le logo de la marque
+    tab[1] = gtk_label_new(model_car); // Initialise le modele de voiture
+    tab[2] = gtk_button_new_with_label ("Show"); // Initialise le bouton
 
     gtk_widget_set_tooltip_text(tab[2], number);
 
     gtk_box_pack_start (GTK_BOX(car[i]),tab[0],TRUE,TRUE,1); // ajoute a la gtkBox le logo
     gtk_box_pack_start (GTK_BOX(car[i]),tab[1],TRUE,TRUE,1); // ajoute a la gtkBox le modele
-    gtk_box_pack_start (GTK_BOX(car[i]),tab[2],TRUE,TRUE,1); // ajoute a la gtkBox le bouton
+    gtk_box_pack_start (GTK_BOX(car[i]),tab[2],FALSE,FALSE,0); // ajoute a la gtkBox le bouton
 
     gtk_container_add(GTK_CONTAINER(box_car), car[i]); // Ajoute toutes les voitures a afficher dans la GtkBox scrollable
 
     g_signal_connect(G_OBJECT (tab[2]), "clicked", G_CALLBACK(print_hello), var); // vérifie que le clic sur le bouton show fonctionne
     g_signal_connect(G_OBJECT (tab[2]), "clicked", G_CALLBACK(edit_label), var); // vérifie que le clic sur le bouton show fonctionne
-  }
+    i++;
+    ptr = ptr->suivant;
+
+    free(model_car);
+    free(brand_car);
+    }
 
 }
 
@@ -188,11 +236,12 @@ int main (int argc, char ** argv)
     gtk_builder_add_from_file (p_builder, "IHM.glade", & p_err);
 
     var = (variable*) malloc(sizeof(variable));
-    InitVar(var);
 
     ptrClient = initializeClients("files/clients.csv");
     ptrReservation = initializeReservation("files/booking.csv", ptrClient);
     ptrCar = initializeCar("files/vehicules.csv", ptrReservation);
+
+    InitVar(var, ptrCar);
 
     GenerateVehicule(var,p_builder, ptrCar);
 
