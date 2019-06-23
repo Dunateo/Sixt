7bc1d16f1051444b1121a4424a67f8af9086ba07
yncrea-hugo <bruna_h@orange.fr>
2019-06-23 11:24:09 +0200

    [FINISHED]Fixing bug merge by integration IHM pierre et IHM mathias

--- IHM.c
+++ IHM.c
@@ -153,7 +153,7 @@ void GenerateVehicule(variable*var, GtkBuilder*p_builder, maillon*ptrTete ) {
   GtkWidget*box_car = NULL; // La GtkBox contenant toute les voitures
   GtkWidget*car[DIM]; // Correspond au voiture a afficher, elle est ajouté dans box_car
   GtkWidget*tab[2];// Contient le contenu de GtkBox  de véhicule
-  char number[4]; // Recupere le nombre de voiture pour le concaténer
+  char* number = (char*)calloc(4, sizeof(char)); // Recupere le nombre de voiture pour le concaténer
   maillon*ptr = ptrTete;
   char*model_car;
   char*brand_car;
@@ -205,6 +205,7 @@ void GenerateVehicule(variable*var, GtkBuilder*p_builder, maillon*ptrTete ) {
     free(model_car);
     free(brand_car);
     }
+  free(number);
 
 }
 
